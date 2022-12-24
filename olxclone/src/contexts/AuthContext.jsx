b51c152c178b92d42602a679acd98f5fad2f9184
Aziz soorti <hies.aziz2019@gmail.com>
2022-12-23 22:16:05 -0800

    working start again

--- olxclone/src/contexts/AuthContext.jsx
+++ olxclone/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import React, { createContext, useState } from "react";
+
 
 export const AuthContext = createContext();
 
@@ -9,9 +9,7 @@ const AuthContextProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
 
   return (
-    <AuthContext.Provider value={
-      {auth,setAuth,API_URL}
-      }>
+    <AuthContext.Provider value={ {auth,setAuth,API_URL} } >
       {children}
     </AuthContext.Provider>
   );
